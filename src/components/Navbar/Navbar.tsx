dd7abf2a43b1ffb814ea1e2519003b3710c940fd
SPK80 <psukonkin@gmail.com>
2022-06-17 06:59:25 +0300

    refactor: change Navbar styles and markup

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,24 +3,27 @@ import s from './Navbar.module.css';
 import {NavLink} from "react-router-dom";
 
 export const Navbar = () => {
+    
+    const navClassNameGetter = (navData: { isActive: boolean }) =>
+        navData.isActive ? s.nav + ' ' + s.active : s.nav
+    
     return (
         <nav className={s.navbar}>
-            <button>
-                <NavLink
-                    className={(navData) => navData.isActive ? s.active : ''}
-                    to={'/profile'}
-                >
-                    Profile
-                </NavLink>
-            </button>
-            <button>
-                <NavLink
-                    className={(navData) => navData.isActive ? s.active : ''}
-                    to={'/dialogs'}
-                >
-                    Dialogs
-                </NavLink>
-            </button>
+            
+            <NavLink
+                className={navClassNameGetter}
+                to={'/profile'}
+            >
+                Profile
+            </NavLink>
+            
+            <NavLink
+                className={navClassNameGetter}
+                to={'/dialogs'}
+            >
+                Dialogs
+            </NavLink>
+        
         </nav>
     
     )
