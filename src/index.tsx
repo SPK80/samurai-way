9be78fe3479918c286713b7b1940ae9fce13f047
SPK80 <psukonkin@gmail.com>
2022-06-20 22:35:29 +0300

    refactor: change creating root container (ReactDOM.render is no longer supported in React 18)

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
 import {BrowserRouter} from "react-router-dom";
 import {store} from "./redux/redux-store";
 import {Provider} from "react-redux";
+import {createRoot} from "react-dom/client";
+
+const container = document.getElementById('root')
+const root = createRoot(container!)
 
 const renderTree = () => {
-    ReactDOM.render(
+    root.render(
         <BrowserRouter>
             <Provider store={store}>
                 <App/>
             </Provider>
         </BrowserRouter>
-        , document.getElementById('root')
     )
 }
 
