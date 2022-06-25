591069e0da1b0f329086c78a8aefe8e16d230528
SPK80 <psukonkin@gmail.com>
2022-06-25 06:43:53 +0300

    docs: add Messages.stories

---
+++ src/stories/Messages.stories.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {Message} from "../components/content/Dialogs/Message";
+import {Messages} from "../components/content/Dialogs/Messages";
+
+export default {
+    title: 'Messages',
+    component: Messages,
+}
+
+export const ThreeMessages = () => (
+    <Messages
+        messages={[
+            {id: '1', text: 'First message'},
+            {id: '2', text: 'This is second message'},
+            {id: '3', text: 'And last message'},
+        ]}
+    />
+)
+
+export const EmptyMessages = () => (
+    <Messages
+        messages={[]}
+    />
+)
