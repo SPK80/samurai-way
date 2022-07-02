b2dc64ebfb434ec72921faec397bfe2a17ff8efc
SPK80 <psukonkin@gmail.com>
2022-07-02 19:26:04 +0300

    fix: add usersPageReducer to rootReducer

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,10 +1,12 @@
 import {combineReducers, legacy_createStore} from "redux";
 import {dialogsPageReducer} from "./reducers/dialogsPageReducer";
 import {profilePageReducer} from "./reducers/profilePageReducer";
+import {usersPageReducer} from "./reducers/usersPageReducer";
 
 const rootReducer = combineReducers({
     dialogsPage: dialogsPageReducer,
     profilePage: profilePageReducer,
+    usersPage: usersPageReducer
 })
 
 export const store = legacy_createStore(rootReducer)
