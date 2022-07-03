87fdaea3feb9e14c226d3f3b32ea0f8052306a1a
SPK80 <psukonkin@gmail.com>
2022-07-03 10:00:38 +0300

    refactor: useSelector useDispatch in UsersPage

--- src/components/content/Users/UsersPage.tsx
+++ src/components/content/Users/UsersPage.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import {UsersListContainer} from "./UsersContainer";
+import {useDispatch, useSelector} from "react-redux";
+import {UsersList} from "./UsersList";
+import {followUserAC, setUsersAC, unfollowUserAC} from "../../../redux/reducers/usersPageActionCreators";
+import {AppStateType} from "../../../redux/redux-store";
+import {UserType} from "../../../redux/reducers/usersPageReducer";
 
 export const UsersPage: React.FC = () => {
+    
+    const usersListState = useSelector<AppStateType, Array<UserType>>(state => state.usersPage.usersList)
+    
+    const dispatch = useDispatch()
+    const dispatchFollow = (userId: string) => dispatch(followUserAC(userId))
+    const dispatchUnfollow = (userId: string) => dispatch(unfollowUserAC(userId))
+    const dispatchSetUsers = (usersList: Array<UserType>) => dispatch(setUsersAC(usersList))
+    
     return (
         <div>
             <h1>Users</h1>
-            <UsersListContainer/>
+            <UsersList
+                usersList={usersListState}
+                follow={dispatchFollow}
+                unfollow={dispatchUnfollow}
+                setUsers={dispatchSetUsers}
+            />
         </div>
     )
 }
