4931c3aeb62023b6e0f6b23c755984f6075f14cf
SPK80 <psukonkin@gmail.com>
2022-07-03 09:44:14 +0300

    refactor: useSelector useDispatch in DialogsPage

--- src/components/content/Dialogs/DialogsPage.tsx
+++ src/components/content/Dialogs/DialogsPage.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import s from './Dialogs.module.css'
-import {DialogsContainer} from "./DialogsContainer";
-import {MessagesContainer} from "./MessagesContainer";
-import {NewMessageContainer} from "./NewMessageContainer";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStateType} from "../../../redux/redux-store";
+import {NewMessage} from "./NewMessage";
+import {addMessageAC, changeNewMessageTextAC} from "../../../redux/reducers/dialogsPageActionCreators";
+import {Dialogs} from "./Dialogs";
+import {DialogType, MessageType} from "../../../redux/reducers/dialogsPageReducer";
+import {Messages} from "./Messages";
 
 export const DialogsPage: React.FC = () => {
+    
+    const newMessageTextState = useSelector<AppStateType, string>(state => state.dialogsPage.newMessageText)
+    const dialogsState = useSelector<AppStateType, Array<DialogType>>(state => state.dialogsPage.dialogs)
+    const messagesState = useSelector<AppStateType, Array<MessageType>>(state => state.dialogsPage.messages)
+    
+    const dispatch = useDispatch()
+    const dispatchChangeNewMessageText = (text: string) => dispatch(changeNewMessageTextAC(text))
+    const dispatchAddMessage = () => dispatch(addMessageAC())
+    
     return (
         <div>
-            <NewMessageContainer/>
+            <NewMessage
+                newMessageText={newMessageTextState}
+                changeMessage={dispatchChangeNewMessageText}
+                addMessage={dispatchAddMessage}
+            />
             <div className={s.dialogs}>
-                <DialogsContainer/>
-                <MessagesContainer/>
+                <Dialogs
+                    dialogs={dialogsState}
+                />
+                <Messages
+                    messages={messagesState}
+                />
             </div>
         </div>
     )
