ed603a414bc72fb40acc123f9b4b13f428d6413f
SPK80 <psukonkin@gmail.com>
2022-07-03 06:00:20 +0300

    refactor: update PavelUser in User.stories

--- src/stories/User.stories.tsx
+++ src/stories/User.stories.tsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import {User} from "../components/content/Users/User";
+import {UserType} from "../redux/reducers/usersPageReducer";
+import {action} from "@storybook/addon-actions";
 
 export default {
     title: 'User',
     component: User,
 }
 
-export const PavelUser = () => (
-    <User
-        name={'Pavel'}
-        avatar={'https://avatars.githubusercontent.com/u/36849366?v=4'}
-        status={'Study React-Redux'}
-        location={{country: "Rus", city: "Moscow"}}
-        following={false}
-        id={'1'}
-    />
-)
+export const PavelUser = () => {
+    const [userData, setUserData] = useState<UserType>({
+        id: '1',
+        avatar: 'https://avatars.githubusercontent.com/u/36849366?v=4',
+        following: false,
+        location: {country: "Rus", city: "Moscow"},
+        status: 'Study React-Redux',
+        name: 'Pavel',
+    })
+    
+    return (
+        <User
+            userData={userData}
+            follow={(id) => {
+                setUserData({...userData, following: true})
+                action("follow")
+            }}
+            unfollow={(id) => {
+                setUserData({...userData, following: false})
+                action("unfollow")
+            }}
+        />
+    )
+}
