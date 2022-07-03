165529423c8ff7ad5b0749821b88b2b2f69be981
SPK80 <psukonkin@gmail.com>
2022-07-03 11:39:22 +0300

    refactor: UsersPage, UsersList

--- src/components/content/Users/UsersList.tsx
+++ src/components/content/Users/UsersList.tsx
@@ -2,69 +2,65 @@ import {UserType} from "../../../redux/reducers/usersPageReducer";
 import React, {useEffect} from "react";
 import {User} from "./User";
 import avatar from "../../../avatar.png";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStateType} from "../../../redux/redux-store";
+import {followUserAC, setUsersAC, unfollowUserAC} from "../../../redux/reducers/usersPageActionCreators";
 
-export type UsersListPropsType = {
-    usersList: Array<UserType>
-    follow: (userId: string) => void
-    unfollow: (userId: string) => void
-    setUsers: (users: Array<UserType>) => void
+export const UsersList: React.FC = () => {
+    useEffect(() => {
+        if (usersListState.length === 0) dispatchSetUsers([
+            {
+                id: '1',
+                name: 'Dmitry K.',
+                avatarUrl: avatar,
+                following: false,
+                status: 'I am looking for a Job right now..',
+                location: {
+                    country: 'Belarus',
+                    city: 'Minsk'
+                }
+            },
+            {
+                id: '2',
+                name: 'Svetlana D.',
+                avatarUrl: avatar,
+                following: false,
+                status: 'I am so pretty',
+                location: {
+                    country: 'Belarus',
+                    city: 'Minsk'
+                }
+            },
+            {
+                id: '3',
+                name: 'Sergei S.',
+                avatarUrl: avatar,
+                following: false,
+                status: 'I like football!',
+                location: {
+                    country: 'Ukraine',
+                    city: 'Kiev'
+                }
+            },])
+    }, [])
+    
+    const usersListState = useSelector<AppStateType, Array<UserType>>(state => state.usersPage.usersList)
+    
+    const dispatch = useDispatch()
+    const dispatchFollow = (userId: string) => dispatch(followUserAC(userId))
+    const dispatchUnfollow = (userId: string) => dispatch(unfollowUserAC(userId))
+    const dispatchSetUsers = (usersList: Array<UserType>) => dispatch(setUsersAC(usersList))
+    
+    return (
+        <div>
+            {usersListState.map(u =>
+                <User
+                    key={u.id}
+                    userData={u}
+                    follow={dispatchFollow}
+                    unfollow={dispatchUnfollow}
+                />)}
+            <button>Show more</button>
+        </div>
+    )
 }
-
-export const UsersList: React.FC<UsersListPropsType> =
-    ({
-         usersList,
-         follow,
-         unfollow,
-         setUsers
-     }) => {
-        
-        useEffect(() => {
-            if (usersList.length === 0) setUsers([
-                {
-                    id: '1',
-                    name: 'Dmitry K.',
-                    avatarUrl: avatar,
-                    following: false,
-                    status: 'I am looking for a Job right now..',
-                    location: {
-                        country: 'Belarus',
-                        city: 'Minsk'
-                    }
-                },
-                {
-                    id: '2',
-                    name: 'Svetlana D.',
-                    avatarUrl: avatar,
-                    following: false,
-                    status: 'I am so pretty',
-                    location: {
-                        country: 'Belarus',
-                        city: 'Minsk'
-                    }
-                },
-                {
-                    id: '3',
-                    name: 'Sergei S.',
-                    avatarUrl: avatar,
-                    following: false,
-                    status: 'I like football!',
-                    location: {
-                        country: 'Ukraine',
-                        city: 'Kiev'
-                    }
-                },])
-        }, [])
-        
-        return (
-            <div>
-                {usersList.map(u =>
-                    <User
-                        key={u.id}
-                        userData={u}
-                        follow={follow}
-                        unfollow={unfollow}
-                    />)}
-                <button>Show more</button>
-            </div>
-        )
-    }
--- src/components/content/Users/UsersPage.tsx
+++ src/components/content/Users/UsersPage.tsx
@@ -1,28 +1,9 @@
 import React from 'react';
-import {useDispatch, useSelector} from "react-redux";
 import {UsersList} from "./UsersList";
-import {followUserAC, setUsersAC, unfollowUserAC} from "../../../redux/reducers/usersPageActionCreators";
-import {AppStateType} from "../../../redux/redux-store";
-import {UserType} from "../../../redux/reducers/usersPageReducer";
 
-export const UsersPage: React.FC = () => {
-    
-    const usersListState = useSelector<AppStateType, Array<UserType>>(state => state.usersPage.usersList)
-    
-    const dispatch = useDispatch()
-    const dispatchFollow = (userId: string) => dispatch(followUserAC(userId))
-    const dispatchUnfollow = (userId: string) => dispatch(unfollowUserAC(userId))
-    const dispatchSetUsers = (usersList: Array<UserType>) => dispatch(setUsersAC(usersList))
-    
-    return (
-        <div>
-            <h1>Users</h1>
-            <UsersList
-                usersList={usersListState}
-                follow={dispatchFollow}
-                unfollow={dispatchUnfollow}
-                setUsers={dispatchSetUsers}
-            />
-        </div>
-    )
-}
+export const UsersPage: React.FC = () => (
+    <div>
+        <h1>Users</h1>
+        <UsersList/>
+    </div>
+)
