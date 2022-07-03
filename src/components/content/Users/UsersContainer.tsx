4fc2972ceb8f9b860f148a682e124a3c04a15088
SPK80 <psukonkin@gmail.com>
2022-07-03 07:32:41 +0300

    fix: implement mapDispatchToProps, fix and refactor UsersList

--- src/components/content/Users/UsersContainer.tsx
+++ src/components/content/Users/UsersContainer.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import {AppStateType} from "../../../redux/redux-store";
 import {connect} from "react-redux";
+import {Dispatch} from "redux";
+import {AppStateType} from "../../../redux/redux-store";
 import {UsersList, UsersListPropsType} from "./UsersList";
+import {followUserAC, unfollowUserAC} from "../../../redux/reducers/usersPageActionCreators";
 
-const mapStateToProps = (state: AppStateType): UsersListPropsType => {
-    return ({
-        usersList: state.usersPage.usersList
-    })
-}
+const mapStateToProps = (state: AppStateType) => ({
+    usersList: state.usersPage.usersList,
+})
 
-// const mapDispatchToProps = (dispatch: Dispatch) => ({})
-export const UsersListContainer = connect(mapStateToProps)(UsersList)
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+    follow: (userId: string) => dispatch(followUserAC(userId)),
+    unfollow: (userId: string) => dispatch(unfollowUserAC(userId)),
+})
+
+export const UsersListContainer = connect(mapStateToProps, mapDispatchToProps)(UsersList)
--- src/components/content/Users/UsersList.tsx
+++ src/components/content/Users/UsersList.tsx
@@ -4,11 +4,25 @@ import {User} from "./User";
 
 export type UsersListPropsType = {
     usersList: Array<UserType>
+    follow: (userId: string) => void
+    unfollow: (userId: string) => void
 }
-export const UsersList: React.FC<UsersListPropsType> = ({usersList}) => {
-    return (
-        <div>
-            {usersList.map(u => <User key={u.id} {...u}/>)}
-        </div>
-    )
-}
+
+export const UsersList: React.FC<UsersListPropsType> =
+    ({
+         usersList,
+         follow,
+         unfollow,
+     }) => {
+        return (
+            <div>
+                {usersList.map(u =>
+                    <User
+                        key={u.id}
+                        userData={u}
+                        follow={follow}
+                        unfollow={unfollow}
+                    />)}
+            </div>
+        )
+    }
