01689f4e5baf14e523d46af18c4f8ddaa927f3f6
SPK80 <psukonkin@gmail.com>
2022-07-04 16:59:09 +0300

    refactor: UsersList

--- src/components/content/Users/UsersList.tsx
+++ src/components/content/Users/UsersList.tsx
@@ -4,21 +4,37 @@ import s from "./usersList.module.css"
 import {User} from "./User";
 import {useDispatch, useSelector} from "react-redux";
 import {AppStateType} from "../../../redux/redux-store";
-import {followUserAC, setUsersAC, unfollowUserAC} from "../../../redux/reducers/usersPageActionCreators";
+import {
+    followUserAC,
+    setTotalCountAC,
+    setUsersAC,
+    unfollowUserAC
+} from "../../../redux/reducers/usersPageActionCreators";
 import axios from "axios";
 
 export const UsersList: React.FC = () => {
-    
-    let totalCount = 0
-    
+
+    const usersListState = useSelector<AppStateType, Array<UserType>>(state => state.usersPage.usersList)
+
+    const dispatch = useDispatch()
+    const dispatchFollow = (userId: string) => dispatch(followUserAC(userId))
+    const dispatchUnfollow = (userId: string) => dispatch(unfollowUserAC(userId))
+    const dispatchSetUsers = (usersList: Array<UserType>) => dispatch(setUsersAC(usersList))
+    const dispatchSetTotalCount = (totalCount: number) => dispatch(setTotalCountAC(totalCount))
+    const totalCount = useSelector<AppStateType, number>(state => state.usersPage.totalCount)
+    const pageSize = useSelector<AppStateType, number>(state => state.usersPage.pageSize)
+    const pagesCount = Math.ceil(totalCount / pageSize)
+
     useEffect(() => {
         console.log("UsersList")
         if (usersListState.length > 0) return
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        const url = `https://social-network.samuraijs.com/api/1.0/users?count=${pageSize}`
+        axios.get(url)
             .then(response => {
-                
-                totalCount = response.data.totalCount
-                const error = response.data.error
+                response.data.error && console.error(response.data.error)
+
+                dispatchSetTotalCount(response.data.totalCount)
+
                 dispatchSetUsers(response.data.items.map((it: any): UserType =>
                     ({
                         id: it.id,
@@ -31,26 +47,19 @@ export const UsersList: React.FC = () => {
                 )
             })
     }, [])
-    
-    const usersListState = useSelector<AppStateType, Array<UserType>>(state => state.usersPage.usersList)
-    
-    const dispatch = useDispatch()
-    const dispatchFollow = (userId: string) => dispatch(followUserAC(userId))
-    const dispatchUnfollow = (userId: string) => dispatch(unfollowUserAC(userId))
-    const dispatchSetUsers = (usersList: Array<UserType>) => dispatch(setUsersAC(usersList))
-    
-    const pageSize = 10;
-    const pagesCount = Math.ceil(totalCount / pageSize)
+
+    console.log('totalCount:', totalCount)
     console.log('pagesCount:', pagesCount)
-    
+    console.log('pageSize:', pageSize)
+
     const pages = []
     for (let i = 0; i < pagesCount; i++) {
         pages.push(i)
     }
-    
+
     return (
         <div className={s.usersList}>
-            {pages.map(p => <span>{p}</span>)}
+            {pages.map(p => <span key={p}>{p}</span>)}
             {usersListState.map(u =>
                 <User
                     key={u.id}
