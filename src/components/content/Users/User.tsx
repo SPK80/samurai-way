bd25fc83e1486362af3c611645c6427d6cde6d3f
SPK80 <psukonkin@gmail.com>
2022-07-15 20:49:52 +0300

    refactor: User

--- src/components/content/Users/User.tsx
+++ src/components/content/Users/User.tsx
@@ -2,42 +2,42 @@ import {UserType} from "../../../redux/reducers/usersPageReducer";
 import React from "react";
 import s from './user.module.css'
 import defaultAvatar from "../../../assets/avatar.png";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStateType} from "../../../redux/redux-store";
+import {followUserAC, unfollowUserAC} from "../../../redux/reducers/usersPageActionCreators";
 
 type UserPropsType = {
-    userData: UserType
-    follow: (userId: string) => void
-    unfollow: (userId: string) => void
+    id: string
 }
 
-export const User: React.FC<UserPropsType> =
-    ({
-         userData,
-         follow,
-         unfollow,
-     }) => {
-        const onButtonClickHandler = () => {
-            if (userData.following) unfollow(userData.id)
-            else follow(userData.id)
-        }
-        
-        return (
-            <div className={s.user}>
-                <div className={s.avatarAndFollowContainer}>
-                    <img src={userData.avatarUrl ?? defaultAvatar} alt="avatar"/>
-                    <button
-                        onClick={onButtonClickHandler}
-                    >{userData.following ? "Unfollow" : "Follow"}</button>
+export const User: React.FC<UserPropsType> = ({id}) => {
+    
+    const userData = useSelector<AppStateType, UserType>(state => state.usersPage.usersList.find(u => u.id === id) ?? {} as UserType)
+    const dispatch = useDispatch()
+    
+    const onButtonClickHandler = () => {
+        if (userData.following) dispatch(unfollowUserAC(userData.id))
+        else dispatch(followUserAC(userData.id))
+    }
+    
+    return (
+        <div className={s.user}>
+            <div className={s.avatarAndFollowContainer}>
+                <img src={userData.avatarUrl ?? defaultAvatar} alt="avatar"/>
+                <button
+                    onClick={onButtonClickHandler}
+                >{userData.following ? "Unfollow" : "Follow"}</button>
+            </div>
+            <div className={s.userDataContainer}>
+                <div className={s.nameAndStatus}>
+                    <div className={s.name}>{userData.name}</div>
+                    <div className={s.status}>{userData.status}</div>
                 </div>
-                <div className={s.userDataContainer}>
-                    <div className={s.nameAndStatus}>
-                        <div className={s.name}>{userData.name}</div>
-                        <div className={s.status}>{userData.status}</div>
-                    </div>
-                    <div className={s.location}>
-                        <div>{userData.location.country},</div>
-                        <div>{userData.location.city}</div>
-                    </div>
+                <div className={s.location}>
+                    <div>{userData.location.country},</div>
+                    <div>{userData.location.city}</div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+}
