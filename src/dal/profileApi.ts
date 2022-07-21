0f414eee533fdb52a5d4c3dc4d7908993530cd46
SPK80 <psukonkin@gmail.com>
2022-07-21 06:57:58 +0300

    feat: implement getStatus and isFollow in profileApi

--- src/dal/profileApi.ts
+++ src/dal/profileApi.ts
@@ -3,7 +3,15 @@ import {UserProfileType} from "../bll/reducers/profilePageReducer";
 
 export const profileApi = {
     async getProfile(userId: number) {
-        return instance.get<UserProfileType>(`profile/${userId}`,)
+        return instance.get<UserProfileType>(`profile/${userId}`)
+            .then(value => value.data)
+    },
+    async getStatus(userId: number) {
+        return instance.get<UserProfileType>(`profile/status/${userId}`)
+            .then(value => value.data)
+    },
+    async isFollow(userId: number) {
+        return instance.get<UserProfileType>(`profile/follow/${userId}`)
             .then(value => value.data)
     },
 }
