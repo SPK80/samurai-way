1cd64f88dbf3aa557c88de3176205a9c82c65551
SPK80 <psukonkin@gmail.com>
2022-07-27 06:32:22 +0300

    feat: implement followApi; add API-KEY in instance

---
+++ src/api/followApi.ts
@@ -0,0 +1,18 @@
+import {instance} from "./instance";
+import {parseResponse, ResponseType} from "./parseResponse";
+
+
+export const followApi = {
+    async getFollow(userId: number) {
+        return instance.get<boolean>('follow/' + userId)
+            .then(res => res.data)
+    },
+    async follow(userId: number) {
+        return instance.post<ResponseType<{}>>('follow/' + userId)
+            .then(parseResponse)
+    },
+    async unfollow(userId: number) {
+        return instance.delete<ResponseType<{}>>('follow/' + userId)
+            .then(parseResponse)
+    },
+}
--- src/api/instance.ts
+++ src/api/instance.ts
@@ -3,4 +3,7 @@ import axios from "axios";
 export const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     withCredentials: true,
+    headers: {
+        'API-KEY': 'da2efd32-8cef-4b04-88b8-e6c3eac02241',
+    }
 })
