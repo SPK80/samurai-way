e68471ceab9f9225789da35b3cdf7178147b4bdc
SPK80 <psukonkin@gmail.com>
2022-10-29 16:23:06 +0300

    feat: create styles for UserPost

--- src/features/profilePage/ui/UserPost.tsx
+++ src/features/profilePage/ui/UserPost.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import s from './Profile.module.css'
 import { PostType } from '../bll/profilePageReducer'
 
 export const UserPost: React.FC<PostType> = (props) => {
     return (
-        <div>
-            <div>{props.text}</div>
-            <div>{props.likesCount}</div>
+        <div className={s.post}>
+            <pre>{props.text}</pre>
+            <div>likes: {props.likesCount}</div>
         </div>
     )
 }
