6440a8ba0ef394b65640cf70e06791bf33ab8d35
SPK80 <psukonkin@gmail.com>
2022-10-30 18:27:34 +0300

    refactor: change BrowserRouter to HashRouter

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AppContent } from './AppContent/AppContent'
 import { store } from '../bll/store'
 
 export const App: React.FC = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <AppContent />
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
