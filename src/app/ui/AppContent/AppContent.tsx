4424a3dfaf43090133cbe5c390d5663dada7a80d
SPK80 <psukonkin@gmail.com>
2022-10-31 06:19:38 +0300

    feat: update AppContent create ErrorSnackbar

--- src/app/ui/AppContent/AppContent.tsx
+++ src/app/ui/AppContent/AppContent.tsx
@@ -1,29 +1,100 @@
 import React, { useEffect } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import s from './AppContent.module.css'
+import Container from '@mui/material/Container'
+import Toolbar from '@material-ui/core/Toolbar'
+import MenuIcon from '@material-ui/icons/Menu'
+import {
+    AppBar,
+    Button,
+    CircularProgress,
+    IconButton,
+    LinearProgress,
+    Typography,
+} from '@mui/material'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import { useAppDispatch, useAppSelector } from '../../bll/store'
 import { DialogsPage } from 'features/dialogsPage'
 import { ProfilePage } from 'features/profilePage'
 import { UsersPage } from 'features/usersPage'
-import { Navbar } from '../Navbar/Navbar'
-import { Header } from '../Header/Header'
-import { AuthPage } from 'features/authPage'
+import { AuthPage, logoutTC } from 'features/authPage'
 import { initializeAppTC } from '../../bll/thunks'
+import { RequestStatus } from 'common/types'
+import { ErrorSnackbar } from 'common/components/ErrorSnackbar'
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        title: {
+            flexGrow: 1,
+        },
+        user: {
+            width: 160,
+            display: 'flex',
+            justifyContent: 'space-between',
+        },
+    })
+)
 
 export const AppContent: React.FC = () => {
     const dispatch = useAppDispatch()
+    const isInitialized = useAppSelector((state) => state.app.isInitialized)
+    const requestStatus = useAppSelector((state) => state.app.request.status)
+    const userId = useAppSelector((state) => state.auth.userData?.id)
+    const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn)
+    const userData = useAppSelector((state) => state.auth.userData)
+
+    const classes = useStyles()
 
     useEffect(() => {
         dispatch(initializeAppTC())
     }, [])
 
-    const userId = useAppSelector((state) => state.auth.userData?.id)
+    const logoutHandler = () => dispatch(logoutTC())
+    if (!isInitialized) return <CircularProgress />
 
     return (
         <div className={s.app}>
-            <Header title={"Hello, samurai! Let's go!"} />
-            <Navbar />
-            <div className={s.wrapperContent}>
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton
+                            edge="start"
+                            className={classes.menuButton}
+                            color="inherit"
+                            aria-label="menu"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" className={classes.title}>
+                            Hello, samurai! Let's go!
+                        </Typography>
+                        {isLoggedIn && (
+                            <div className={classes.user}>
+                                <Typography variant="h6">
+                                    {userData?.login}
+                                </Typography>
+                                <Button
+                                    onClick={logoutHandler}
+                                    color="inherit"
+                                    variant={'outlined'}
+                                >
+                                    LogOut
+                                </Button>
+                            </div>
+                        )}
+                    </Toolbar>
+                </AppBar>
+                {requestStatus === RequestStatus.loading && <LinearProgress />}
+            </div>
+
+            <ErrorSnackbar />
+            <Container fixed style={{ margin: 0 }}>
                 <Routes>
                     <Route
                         path={'*'}
@@ -38,7 +109,7 @@ export const AppContent: React.FC = () => {
                     <Route path={'/dialogs/*'} element={<DialogsPage />} />
                     <Route path={'/users'} element={<UsersPage />} />
                 </Routes>
-            </div>
+            </Container>
         </div>
     )
 }
---
+++ src/common/components/ErrorSnackbar.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Snackbar from '@mui/material/Snackbar'
+import MuiAlert, { AlertProps } from '@mui/material/Alert'
+import { useAppSelector } from 'app/bll/store'
+import { useDispatch } from 'react-redux'
+import { setAppErrorAC } from 'app'
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props,
+    ref
+) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+})
+
+export const ErrorSnackbar: React.FC = React.memo(() => {
+    const error = useAppSelector((state) => state.app.request.error)
+    const dispatch = useDispatch()
+
+    const handleClose = (
+        event?: React.SyntheticEvent | Event,
+        reason?: string
+    ) => {
+        if (reason === 'clickaway') return
+        dispatch(setAppErrorAC(null))
+    }
+
+    return (
+        <Snackbar open={!!error} autoHideDuration={6000} onClose={handleClose}>
+            <Alert
+                onClose={handleClose}
+                severity="error"
+                sx={{ width: '100%' }}
+            >
+                {error}
+            </Alert>
+        </Snackbar>
+    )
+})
