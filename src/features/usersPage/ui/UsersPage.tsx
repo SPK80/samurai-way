683713e384834019c47d7a7b8c917fa3249b8142
SPK80 <psukonkin@gmail.com>
2022-11-08 06:01:51 +0300

    feat: update UsersPage layout

--- src/features/usersPage/ui/UsersPage.tsx
+++ src/features/usersPage/ui/UsersPage.tsx
@@ -2,16 +2,16 @@ import React, { memo, useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from 'app'
 import { PagesCounter } from 'common/components/PagesCounter/PagesCounter'
 import { RequestStatus } from 'common/types'
-import { Progress } from 'common/components/Progress/Progress'
 import { UsersList } from './UsersList'
 import { fetchUsersTC } from '../bll/thunks'
 import { setCurrentPageAC, setPageSizeAC } from '../bll/actions'
+import Paper from '@mui/material/Paper'
 
 export const UsersPage: React.FC = memo(() => {
     const { totalCount, pageSize, currentPage } = useAppSelector(
         (state) => state.usersPage
     )
-    const { status, error } = useAppSelector((state) => state.app.request)
+    const { status } = useAppSelector((state) => state.app.request)
     const dispatch = useAppDispatch()
 
     useEffect(() => {
@@ -27,20 +27,24 @@ export const UsersPage: React.FC = memo(() => {
         dispatch(setCurrentPageAC(newCurrentPage))
 
     return (
-        <div>
+        <Paper sx={{ minHeight: '100%', p: 1, overflow: 'hidden' }}>
             <h1>Users</h1>
-            <PagesCounter
-                pageSize={pageSize}
-                pageSizeVariants={[5, 10, 100]}
-                currentPage={currentPage}
-                totalCount={totalCount}
-                onPageSizeSelected={onPageSizeSelectedHandler}
-                onCurrentPageChanged={onCurrentPageChangedHandler}
-            />
-            {status === RequestStatus.loading && <Progress />}
-            {error && <h3 color={'red'}>{error}</h3>}
-            {status === RequestStatus.idle && <UsersList />}
-            <button onClick={onClickHandler}>Show more</button>
-        </div>
+            {status === RequestStatus.loading ? (
+                <div>Loading...</div>
+            ) : (
+                <>
+                    <PagesCounter
+                        pageSize={pageSize}
+                        pageSizeVariants={[5, 10, 100]}
+                        currentPage={currentPage}
+                        totalCount={totalCount}
+                        onPageSizeSelected={onPageSizeSelectedHandler}
+                        onCurrentPageChanged={onCurrentPageChangedHandler}
+                    />
+                    <UsersList />
+                    <button onClick={onClickHandler}>Show more</button>
+                </>
+            )}
+        </Paper>
     )
 })
