ce18d9558bd675f9b385b15f45c076437d39edcb
SPK80 <psukonkin@gmail.com>
2022-12-06 14:19:59 +0300

    fix: User.stories

--- src/stories/User.stories.tsx
+++ src/stories/User.stories.tsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import { ReduxStoreProviderDecorator } from './decorators/ReduxStoreProviderDecorator'
+import React, { useState } from 'react'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { BrowserRouterDecorator } from './decorators/BrowserRouterDecorator'
-import { initialRequestingState } from '../common/bll/types'
-import { User } from '../features/usersPage/ui/User'
+import { initialRequestingState } from 'common/bll/types'
+import { User } from 'features/usersPage/ui/User'
+import { action } from '@storybook/addon-actions'
 
 export default {
     title: 'User',
     component: User,
-    decorators: [BrowserRouterDecorator, ReduxStoreProviderDecorator],
+    decorators: [BrowserRouterDecorator],
 } as ComponentMeta<typeof User>
 
-const Template: ComponentStory<typeof User> = (args) => <User userData={args.userData} />
+const Template: ComponentStory<typeof User> = (args) => {
+    const [userData, setUserData] = useState(args.userData)
+
+    const onFollowHandler = (userId: number, isFollow: boolean) => {
+        action('onFollow ' + JSON.stringify({ userId, isFollow }))
+        setUserData((ud) => ({
+            ...ud,
+            followed: !ud.followed,
+        }))
+    }
+
+    return <User userData={userData} onFollow={onFollowHandler} />
+}
 
 export const UserStory = Template.bind({})
 UserStory.args = {
