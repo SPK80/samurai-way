746b6fc471f9b92acf7e1c19f0f55c66b92b7419
SPK80 <psukonkin@gmail.com>
2022-12-15 16:54:51 +0300

    refactor: AppRoutes

--- src/app/ui/AppContent/AppRoutes.tsx
+++ src/app/ui/AppContent/AppRoutes.tsx
@@ -4,59 +4,55 @@ import { AuthPage } from 'features/authPage'
 import { ProfilePage } from 'features/profilePage'
 import { DialogsPage } from 'features/dialogsPage'
 import { UsersPage } from 'features/usersPage'
-import { useAppSelector } from '../../bll/store'
 import { LoginRedirect } from './LoginRedirect'
 
 export enum Path {
     Root = '/',
-    Other = '/*',
-    AuthPage = 'authPage',
-    Profile = 'profile',
-    Dialogs = 'dialogs',
-    Users = 'users',
-    PageNotFound = '404',
+    Other = '*',
+    AuthPage = '/authPage',
+    Profile = '/profile',
+    Dialogs = '/dialogs',
+    Users = '/users',
+    PageNotFound = '/404',
 }
 
+const routes = [
+    { path: Path.AuthPage, element: <AuthPage /> },
+    {
+        path: Path.Profile,
+        element: (
+            <LoginRedirect>
+                <ProfilePage />
+            </LoginRedirect>
+        ),
+    },
+    {
+        path: Path.Profile + '/:userId',
+        element: (
+            <LoginRedirect>
+                <ProfilePage />
+            </LoginRedirect>
+        ),
+    },
+    {
+        path: Path.Dialogs + '/*',
+        element: (
+            <LoginRedirect>
+                <DialogsPage />
+            </LoginRedirect>
+        ),
+    },
+    { path: Path.Users, element: <UsersPage /> },
+    { path: Path.PageNotFound, element: <h1>404</h1> },
+    { path: Path.Other, element: <Navigate to={Path.PageNotFound} /> },
+]
+
 export const AppRoutes: React.FC = () => {
-    // const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn)
     return (
         <Routes>
-            {/*<Route*/}
-            {/*    path={Path.Root}*/}
-            {/*    element={<Navigate to={isLoggedIn ? Path.Profile : Path.AuthPage} />}*/}
-            {/*/>*/}
-            <Route path={Path.AuthPage} element={<AuthPage />} />
-
-            <Route
-                path={Path.Profile}
-                element={
-                    <LoginRedirect>
-                        <ProfilePage />
-                    </LoginRedirect>
-                }
-            />
-
-            {/*<Route*/}
-            {/*    path={Path.Profile + '/:userId'}*/}
-            {/*    element={*/}
-            {/*        <LoginRedirect>*/}
-            {/*            <ProfilePage />*/}
-            {/*        </LoginRedirect>*/}
-            {/*    }*/}
-            {/*/>*/}
-
-            {/*<Route*/}
-            {/*    path={Path.Dialogs + '/*'}*/}
-            {/*    element={*/}
-            {/*        <LoginRedirect>*/}
-            {/*            <DialogsPage />*/}
-            {/*        </LoginRedirect>*/}
-            {/*    }*/}
-            {/*/>*/}
-
-            {/*<Route path={Path.Users} element={<UsersPage />} />*/}
-            <Route path={Path.PageNotFound} element={<h1>404</h1>} />
-            <Route path={'*'} element={<Navigate to={Path.PageNotFound} />} />
+            {routes.map((r) => (
+                <Route {...r} />
+            ))}
         </Routes>
     )
 }
